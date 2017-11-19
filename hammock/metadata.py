b0d606aeb440e6c5e96b8f0412d1dcfb53417928
Lemuel Formacil <lemuelf@gmail.com>
2017-11-20 00:30:26 +0400

    Add `StateTransitioningFieldMetadataMixin` metadata class mixin
    Mixin is for metadata of serializers with a state transitioning field.

--- hammock/metadata.py
+++ hammock/metadata.py
@@ -62,3 +62,71 @@ class SimplerMetadata(metadata.SimpleMetadata):
             ]
 
         return field_info
+
+
+class StateTransitioningFieldMetadataMixin(object):
+    """Metadata class mixin for serializers with state transitioning field."""
+
+    def get_field_info(self, field):
+        """Return field info."""
+        field_info = super(
+            StateTransitioningFieldMetadataMixin, self).get_field_info(field)
+
+        if (not field_info.get('read_only')
+                and isinstance(field, serializers.ChoiceField)):
+            # Field is a writable ChoiceField
+            # Describe the choices
+
+            field_info['choices'] = self.get_field_choices_info(
+                field, field_name=field.field_name)
+
+        return field_info
+
+    def _get_valid_transition_previous_states(self, model, state):
+        """Return valid previous states to be able to transition to `state`."""
+        try:
+            # return list of states `state` can transition from
+            return next(
+                t[1]
+                for t in model.STATE_TRANSITIONS
+                if state == t[0])
+        except StopIteration:
+            # `state` is not listed in `STATE_TRANSITIONS`, meaning
+            # it is allowed to transition from any other state;
+            # return all states except value of `state`
+
+            # get states that can't be transitioned out of
+            fixed_states = list(dict(model.STATE_TRANSITIONS).get(None, []))
+
+            return [
+                choice[0]
+                for choice in next(
+                    field
+                    for field in model._meta.fields
+                    if field.name == 'state'
+                ).choices
+                if choice[0] not in fixed_states + [state]
+            ] + [None]
+
+    def get_field_choices_info(self, field, field_name=None):
+        """Return choices metadata dictionary for field."""
+        choices = []
+        for choice_value, choice_name in field.choices.items():
+            choice = OrderedDict([
+                ('value', choice_value),
+                ('display_name', force_text(choice_name, strings_only=True)),
+            ])
+
+            # Set choice validity conditions for `state` field
+            if (hasattr(field.parent.Meta.model, 'STATE_TRANSITIONS')
+                    and field_name == 'state'):
+                choice['valid'] = {
+                    'only_if': {
+                        'state': self._get_valid_transition_previous_states(
+                            field.parent.Meta.model, choice_value),
+                    }
+                }
+
+            choices.append(choice)
+
+        return choices
