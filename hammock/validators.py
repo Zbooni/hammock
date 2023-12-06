686895f4e1bc1b8bd79cb68419d4cda0973005fd
Francis John Quiachon <francis@zbooni.com>
2023-12-06 15:52:28 +0800

    Update hammock django.utils.translation import ugettext_lazy to gettext_lazy

--- hammock/validators.py
+++ hammock/validators.py
@@ -1,7 +1,7 @@
 """Validator classes for serializers and serializer fields."""
 
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import ValidationError
 from rest_framework.utils.representation import smart_repr
